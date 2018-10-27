76f845ba4b068a20822210bdcfc4f3bc2db94940
SandraSILK <mrs0sandwich@gmail.com>
2018-10-27 13:00:04 +0200

    added styles to login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,18 +2,17 @@
 
 @section('content')
 <main>
-    <div class="img-hero text-center p-top-25vh">
+    <div class="img-hero p-top-25vh">
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-8 login">
-                    <h6>Logowanie</h6>
+                    <h6 class="text-center">Logowanie</h6>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">Adres e-mail</label>
-
-                            <div class="col-md-6">
+                        <div class="form-group justify-content-center row">
+                            <label for="email" class="col-md-10 col-form-label">Adres e-mail</label>
+                            <div class="col-md-10">
                                 <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
@@ -24,10 +23,10 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Hasło</label>
+                        <div class="form-group justify-content-center row">
+                            <label for="password" class="col-md-10 col-form-label">Hasło</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-10">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
@@ -38,7 +37,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <div class="form-group row text-center">
                             <div class="col-md-6">
                                 <div class="checkbox">
                                     <label>
@@ -49,17 +48,22 @@
                         </div>
 
                         <div class="form-group row justify-content-center">
-                            <div class="col-5">
+                            <div class="col-md-5">
                                 <button type="submit" class="btn btn-outline-light w-100">
                                     Zaloguj
                                 </button>
                             </div>
-                            <div class="col-3">
+                            <div class="col-md-3">
                                 <a class="btn btn-link badge-light" href="{{ route('password.request') }}">Nie pamiętasz hasła?
                                 </a>
                             </div>
                         </div>
                     </form>
+                    <div class="row m-b-40 justify-content-center">
+                        <div class="col-md-3 offset-md-5">
+                            <a class="btn btn-link badge-light" href="/register">Nie masz konta? Załóż!</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
